3dc00632363599961544e1a734ab41c1c5bc0f1a
Michikawa Masayoshi <michikawa.masayoshi@gmail.com>
2015-03-06 17:41:11 +0900

    Modified double quote to single quote

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-fonts_dir = "font"
-images_dir = "img"
-javascripts_dir = "js"
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+fonts_dir = 'font'
+images_dir = 'img'
+javascripts_dir = 'js'
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
